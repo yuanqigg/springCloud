5118b7d71df423fdde8b8333ef09f393ab0a00ac
qi.yuan <yuan_qi@cathaypacific.com>
2018-05-31 18:50:39 +0800

    add configserver

--- configserver/src/main/java/com/learn/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/learn/configserver/ConfigserverApplication.java
@@ -2,12 +2,14 @@ package com.learn.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
 @RestController
+@EnableDiscoveryClient
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {

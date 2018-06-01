1709c6846660665ab5696c83b2e264e6741b9429
qi.yuan <yuan_qi@cathaypacific.com>
2018-06-01 18:43:21 +0800

    config client && server

--- configclient/src/main/java/com/learn/myself/configclient/controller/MySampleRestController.java
+++ configclient/src/main/java/com/learn/myself/configclient/controller/MySampleRestController.java
@@ -3,6 +3,7 @@ package com.learn.myself.configclient.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RefreshScope
 public class MySampleRestController {
+    @Autowired
+    private Environment env;
 
+    @Value("${foo}")
+    private String foo;
+
+    @RequestMapping("/get")
+    public String getAppName() {
+        return env.getProperty("foo", "undefined");
+    }
 }
